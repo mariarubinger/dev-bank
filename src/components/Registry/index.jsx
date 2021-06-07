b4bcd91667281172d7274caeddb8cd1ca8d3029b
Maria Clara Rubinger <rubinger.maria@gmail.com>
2021-06-07 15:22:09 +0100

    Add firebase package and create firebase config

--- src/components/Registry/index.jsx
+++ src/components/Registry/index.jsx
@@ -1,16 +1,54 @@
 import { Container, Content, Card } from './styles';
+import * as FirestoreService from '../../services/firestore';
+import { useState } from 'react';
 
 export function Registry(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function addUser(e){
+      if(!name || !email || !password) return;
+        e.preventDefault();  
+        FirestoreService.createUser(name, email, password);
+        setName('');
+        setEmail('');
+        setPassword('');
+    }
+    
     return(
         <Container>
             <Content>
                 <h1>O banco feito especialmente para <span>Desenvolvedores</span></h1>
                 <Card>
                     <h3>Crie sua conta</h3>
-                    <input placeholder="Nome"></input>
-                    <input placeholder="E-mail"></input>
-                    <input placeholder="Senha"></input>
-                    <button type="button">Cadastrar</button>  
+                    <input
+                        type="text"
+                        placeholder="Nome"
+                        className=""
+                        onChange={e => setName(e.target.value)}
+                        value={name}
+                        />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        className=""
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Senha"
+                        className=""
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
+                    />
+                    <button
+                        type="submit"
+                        className=""
+                        onClick={addUser}>
+                        Cadastrar
+                    </button>  
                 </Card>
             </Content>
         </Container>
---
+++ src/services/firestore.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/performance";
+import "firebase/firestore";
+import "firebase/auth";
+ 
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+};
+ 
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+ 
+const db = firebase.firestore();
+ 
+export const authenticateAnonymously = () => {
+    return firebase.auth().signInAnonymously();
+};
+
+ 
+export default firebase;
